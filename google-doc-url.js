472bab7d01540ebf56f023b72ea80908133199ef
gomerland <61566531+gomerland@users.noreply.github.com>
2020-02-28 08:59:39 -0800

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/13vSVXJnmFgFzY68wP-4xDwfn2HIjtl260ijvlk4husA/edit#gid=0';
